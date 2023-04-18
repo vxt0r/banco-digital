8391f0088fa62a484c6775423cc913ed6434ebc0
Vitor <vitormoroli@gmail.com>
2023-04-17 21:28:19 -0300

    ajustando envio de formulário vazio na tela cadastro

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -12,10 +12,12 @@ class AuthController extends Action{
     public function cadastrar(){
         $autenticacao = Container::getModel('autenticacao');
 
-        if(!empty($_POST)){
-            if(isset($_POST['nome']) && isset($_POST['email']) && isset($_POST['senha'])){
-                $autenticacao->cadastrar($_POST['nome'],$_POST['email'],$_POST['senha']);
-            }
+        if(isset($_POST['nome']) && isset($_POST['email']) && isset($_POST['senha'])){
+          if(empty($_POST['nome']) || empty($_POST['email']) || empty($_POST['senha'])){
+            header('location: cadastrar?erro=1');
+            exit();
+          }
+          $autenticacao->cadastrar($_POST['nome'],$_POST['email'],$_POST['senha']);
         }
 
         $this->view->dados = $_SERVER['REQUEST_URI'];
@@ -25,15 +27,17 @@ class AuthController extends Action{
     public function login(){
         $autenticacao = Container::getModel('autenticacao');
 
-        if(!empty($_POST)){
-            if(isset($_POST['email']) && isset($_POST['senha'])){
-                $autenticacao->login($_POST['email'],$_POST['senha']);
-            }
+        if(isset($_POST['email']) && isset($_POST['senha'])){
+          if(empty($_POST['email']) || empty($_POST['senha'])){
+            header('location: login?erro=1');
+            exit();
+          }
+          $autenticacao->login($_POST['email'],$_POST['senha']);
         }
-        
+
         $this->view->dados = $_SERVER['REQUEST_URI'];
         $this->render('login','layout');
     }
 
-   
+
 }
