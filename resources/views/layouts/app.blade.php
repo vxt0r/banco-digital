10f02b54e7f0da5a650b13b0d63e166e93c00ef1
Vitor Moreira de Oliveira <vitormoroli@gmail.com>
2023-09-04 12:09:16 -0300

    mundando cor de fundo do menu hamburguer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,7 +26,7 @@
                     {{-- {{ config('app.name', 'Laravel') }} --}}
                     Seu Banco Digital
                 </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <button class="navbar-toggler bg-secondary" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
